126e001446940d7e3bbf5c2696fc60881f4b2bb0
Sm0kE-by <Necossby@gmail.com>
2025-12-06 10:05:59 +0300

    Лямбда-выражения и тип функции. стр.241-280

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,32 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+
+    narrate("A hero enters the town of Kronstadt. What is their name?",
+        //ссылка на ф-ию
+        ::makeYellow)
+
+    val heroName = readLine()
+    require(heroName != null && heroName.isNotEmpty()) {
+        "The hero must have a name."
+    }
+
+    changeNarratorMood()
+    narrate("$heroName, ${createTitle(heroName)}, heads to the town square")
+}
+
+// Выводит сообщение желтым цветом
+private fun makeYellow(message: String) =
+    "\u001b[33;1m$message\u001b[0m"
+
+private fun createTitle(name: String): String {
+    return when {
+        //имя состоит только из цифр
+        name.all { it.isDigit() } -> "The Identifiable"
+        //имя не содержит ни одной буквы
+        name.none { it.isLetter() } -> "The Witness Protection Member"
+        //имя содержит много гласных
+        name.count { it.lowercase() in "aeiou" } > 4 -> "The Master of Vowel"
+        else -> "The Renowned Hero"
+    }
 }
---
+++ src/main/kotlin/Narrator.kt
@@ -0,0 +1,58 @@
+package org.example
+
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+var narrationModifier: (String) -> String = { it }
+
+//К счастью, есть возможность включить оптимизацию,
+//которая избавит от лишних затрат памяти при использовании
+//лямбда-функций в качестве аргументов. Такая возможность
+//называется встраиванием (inlining). Встраивание избавляет от
+//необходимости использовать экземпляр объекта и выделять
+//память для переменных в лямбда-функции.
+inline fun narrate(message: String,
+            modifier: (String) -> String = {
+                narrationModifier(it) }) {
+    println(modifier(message))
+}
+
+fun changeNarratorMood() {
+    val mood: String
+    val modifier: (String) -> String
+    when (Random.nextInt(1..5)) {
+        1 -> {
+            mood = "loud"
+            modifier = { message ->
+                val numExclamationPoints = 3
+                message.uppercase() + "!".repeat(numExclamationPoints)
+            }
+        }
+
+        2 -> {
+            mood = "tired"
+            modifier = { message ->
+                message.lowercase().replace(" ", "... ")
+            }
+        }
+
+        3 -> {
+            mood = "unsure"
+            modifier = { message -> "$message?" }
+        }
+        4 -> {
+            var narrationsGiven = 0
+            mood = "like sending an itemized bill"
+            modifier = { message ->
+                narrationsGiven++
+                "$message.\n(I have narrated $narrationsGiven things)"
+            }
+        }
+        else -> {
+            mood = "professional"
+            modifier = { message -> "$message." }
+        }
+    }
+    narrationModifier = modifier
+    narrate("The narrator begins to feel $mood")
+}
